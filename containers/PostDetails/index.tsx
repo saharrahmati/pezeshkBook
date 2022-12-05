a7ea20b21af1c8dcf1df9c01e0025535176acd0b
s.rahmati <sahar.rahmati60@gmail.com>
2022-12-05 19:19:25 +0330

    removed extra import

--- containers/PostDetails/index.tsx
+++ containers/PostDetails/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-//import axios from "axios";
 import { useRouter } from "next/router";
 import Details from "../../components/PostDetails";
 import {DetailsType} from "../../components/PostDetails/types.d"
